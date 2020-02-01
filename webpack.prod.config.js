2639a7845aeb526312e0b6d697e513b7899170d1
Jonathan Diaz <jgdiazcherrez@gmail.com>
2020-02-01 16:59:56 +0100

    Bundle updated

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,23 +1,23 @@
-const  webpack = require('webpack');
-const moduleConfig = require('./module.config');
+const webpack = require('webpack');
 const WebpackBar = require('webpackbar');
 const WebpackNotifierPlugin = require('webpack-notifier');
 const path = require('path');
+const moduleConfig = require('./module.config');
 
 module.exports = {
-    devtool: '', // Removed dev-tools mapping
-    mode: "production",
-    entry: ['./src/index.js'],
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins:[
-        new WebpackBar(),
-        new WebpackNotifierPlugin()
-    ],
-    performance: {
-        hints: false
-    },
-    module:moduleConfig
+  devtool: '', // Removed dev-tools mapping
+  mode: 'production',
+  entry: ['./src/index.js'],
+  output: {
+    filename: 'bundle.min.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new WebpackBar(),
+    new WebpackNotifierPlugin()
+  ],
+  performance: {
+    hints: false
+  },
+  module: moduleConfig
 };
