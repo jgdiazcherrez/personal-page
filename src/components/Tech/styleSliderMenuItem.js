9fe55db8744f1bc73ca91dec4734e2f7e10a3846
Jonathan <jonathan.diaz@elespanol.com>
2020-02-01 17:31:06 +0100

    deleted style tag HTML and added tech style using js

---
+++ src/components/Tech/styleSliderMenuItem.js
@@ -0,0 +1,18 @@
+
+import { makeStyles } from '@material-ui/core/styles';
+import commonStyle from '../style';
+
+export default makeStyles({
+  ...commonStyle,
+  menuItem: {
+    padding: '0 15px',
+    margin: '5px 10px',
+    userSelect: 'none',
+    cursor: 'none',
+    border: 'none',
+    '& img': {
+      width: '250px',
+      height: '150px'
+    }
+  }
+});
