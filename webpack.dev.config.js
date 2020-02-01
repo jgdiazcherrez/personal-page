7ee4acafa2d77b53c082c9d1d219e8c2b83bde60
Jonathan <jonathan.diaz@elespanol.com>
2020-02-01 16:59:56 +0100

    Bundle updated

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -9,36 +9,36 @@ const port = 10000;
 
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-    entry: [
-        `webpack-dev-server/client?http://${host}:${port}`,
-        'webpack/hot/only-dev-server',
-        './src/index.js'
-    ],
-    output: {
-        //filename: '[name].min.js',
-        filename: "bundle.js",
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: `http://${host}:${port}/`
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    `webpack-dev-server/client?http://${host}:${port}`,
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    // filename: '[name].min.js',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: `http://${host}:${port}/`
+  },
+  plugins: [
+    // new WebpackNotifierPlugin(),
+    // new webpack.NamedModulesPlugin()
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port,
+    host,
+    overlay: {
+      errors: true,
+      warnings: true
     },
-    plugins:[
-        //new WebpackNotifierPlugin(),
-        //new webpack.NamedModulesPlugin()
-    ],
-    devServer: {
-        contentBase: path.resolve(__dirname, "dist"),
-        port,
-        host,
-        overlay: {
-            errors: true,
-            warnings: true
-        },
-        historyApiFallback: true,
-        hot:true,
-        disableHostCheck: true,
-        stats: {
-            colors: true
-        }
-    },
-    module: moduleConfig
+    historyApiFallback: true,
+    hot: true,
+    disableHostCheck: true,
+    stats: {
+      colors: true
+    }
+  },
+  module: moduleConfig
 };
